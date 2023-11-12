d014f884c55bc1f08decfe54b1d1b17d272e01ce
janebmckenna <>
2023-11-12 16:15:21 +0000

    func:return data to the user for prep

--- run.py
+++ run.py
@@ -123,6 +123,17 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Displays the prep values for the Love Sandwiches team
+    """
+    stock = SHEET.worksheet('stock').get_all_values()
+    headings = stock[0]
+    dictionary = {}
+    for heading, value in zip(headings, data):
+        dictionary[heading] = value
+    return f'Please Make the following sandwiches for the next market:\n {dictionary}'
+
 def main():
     """
     Run all program functions
@@ -135,6 +146,8 @@ def main():
     sales_columns = get_last_five_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
     
 
 print('Welcome to Love Sandwiches Data Automation')
